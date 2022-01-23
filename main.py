b89babcbd1a5272d640d21b89daa2d09c065dce8
Muqadasa-Sherani <97336771+Muqadasa-Sherani@users.noreply.github.com>
2022-01-23 14:08:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,63 @@ def decide_days(day):
         return 4
     else:
         return -1
+    
+    def get_collision_morning(day, course):
+    flag = False
+
+    if day.morning.big1 == True and day.morning.bigClassroom1Course.get_year() == course.get_year():
+        flag = True
+
+    if day.morning.big2 == True and day.morning.bigClassroom2Course.get_year() == course.get_year():
+        flag = True
+
+    if day.morning.small1 == True and day.morning.smallClassroom1Course.get_year() == course.get_year():
+        flag = True
+
+    if day.morning.small2 == True and day.morning.smallClassroom2Course.get_year() == course.get_year():
+        flag = True
+
+    return flag
+
+
+def get_collision_afternoon(day, course):
+    flag = False
+
+    if day.afternoon.big1 == True and day.afternoon.bigClassroom1Course.get_year() == course.get_year():
+        flag = True
+
+    if day.afternoon.big2 == True and day.afternoon.bigClassroom2Course.get_year() == course.get_year():
+        flag = True
+
+    if day.afternoon.small1 == True and day.afternoon.smallClassroom1Course.get_year() == course.get_year():
+        flag = True
+
+    if day.afternoon.small2 == True and day.afternoon.smallClassroom2Course.get_year() == course.get_year():
+        flag = True
+
+    return flag
+
+
+def is_busy_morning(course, index, busy):
+    flag = False
+    for i in range(len(busy)):
+        busyTimeSlotDayIndex = decide_days(busy[i][1])
+        time = busy[i][2].rstrip("\n")
+        if course.get_instructor() == busy[i][0] and index == busyTimeSlotDayIndex and time == "Morning":
+            flag = True
+            break
+
+    return flag
+
+
+def is_busy_afternoon(course, index, busy):
+    flag = False
+
+    for i in range(len(busy)):
+        busyTimeSlotDayIndex = decide_days(busy[i][1])
+        time = busy[i][2].rstrip("\n")
+        if course.get_instructor() == busy[i][0] and index == busyTimeSlotDayIndex and time == "Afternoon":
+            flag = True
+            break
+
+    return flag
